9310af4e9eb703bba07e2f364d469a1306780ae0
ojasks <ojaskrsingh24@gmail.com>
2025-06-29 16:24:19 +0530

    built a to-do list project

---
+++ 10todocontextLocal/src/contexts/index.js
@@ -0,0 +1,3 @@
+//projects me aksar index index file isliye bnayi jaaati hai taaki ham saare imports idhar le le
+// aur sab yaha se export krna ho to dusri files
+export {TodoContext, TodoProvider, useTodo } from "./TodoContext"
